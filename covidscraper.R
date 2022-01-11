d87408e112ab55b1f71db0857fb73e5ce331eaef
Christian Leonard <xian.leonard@gmail.com>
2022-01-11 13:16:46 -0800

    scraper update

--- covidscraper.R
+++ covidscraper.R
@@ -12,19 +12,19 @@ tables <- lacounty_covid %>%
   html_elements("table") %>% 
   html_table
 
-# Just the second one out of the list of lists
+## Just the second one out of the list of lists
 community_covid <- tables[2]
 
-# Turn that into a dataframe
+## Turn that into a dataframe
 community_covid <- rbind.data.frame(community_covid)
 
-# Clean
+## Clean
 bur_glen <- community_covid %>% 
   clean_names() %>% 
   filter(city_community=="City of Burbank" | city_community == "City of Glendale")
 
-# Need to have date
-
+# Get date
+## Requires headless browser
 writeLines(sprintf("var page = require('webpage').create();
 page.open('%s', function () {
     console.log(page.content);
@@ -33,14 +33,16 @@ page.open('%s', function () {
 
 system("/usr/local/bin/phantomjs scraper.js", timeout=30)
 
+## Import HTML result
 jsscrape <- read_html("lacountycovid.html")
 date <- jsscrape %>% 
   html_elements(xpath="/html/body/div[2]/div[1]/div[1]/div[5]/div/p/span[2]") %>% 
   html_text2()
 
+## Add to DF
 bur_glen$valid_thru <- date
 
-# trims date
+## trims date
 bur_glen$valid_thru <- str_sub(bur_glen$valid_thru, start=-10)
-# as date
+## as date
 bur_glen$valid_thru <- mdy(bur_glen$valid_thru)
